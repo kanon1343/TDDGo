66dc725f7379b6ecd963a0cd86301afdd6b10ee2
kanon1343 <kanon.h1343@gmail.com>
2023-04-13 14:57:48 +0900

    配列とスライス終了

--- sum/sum.go
+++ sum/sum.go
@@ -7,3 +7,24 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, number := range numbersToSum {
+		sums = append(sums, Sum(number))
+	}
+	return sums
+}
+
+func SumAllTails(numberToSum ...[]int) []int {
+	var sums []int
+	for _, number := range numberToSum {
+		if len(number) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := number[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
